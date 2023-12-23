2c47fafaa0e353091d60f3343652e631434772cd
JeonK1 <kan5016@naver.com>
2023-12-23 18:35:54 +0900

    chapter3

--- TDDPlayground/app/src/main/java/com/example/tddplayground/model/Dollar.kt
+++ TDDPlayground/app/src/main/java/com/example/tddplayground/model/Dollar.kt
@@ -1,7 +1,12 @@
 package com.example.tddplayground.model
 
-class Dollar(var amount: Int) {
+class Dollar(val amount: Int) {
     fun times(multiplier: Int): Dollar {
         return Dollar(amount * multiplier)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        val others = other as Dollar
+        return amount == others.amount
     }
 }
--- TDDPlayground/app/src/test/java/com/example/tddplayground/DollarUnitTest.kt
+++ TDDPlayground/app/src/test/java/com/example/tddplayground/DollarUnitTest.kt
@@ -2,22 +2,27 @@ package com.example.tddplayground
 
 import com.example.tddplayground.model.Dollar
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class DollarUnitTest {
     @Test
     fun testMultiplication() {
         val five = Dollar(5)
-        val result = five.times(2)
-        assertEquals(10, result.amount)
+        assertEquals(Dollar(10), five.times(2))
     }
 
     @Test
     fun testMultiplication2() {
         val five = Dollar(5)
-        var result = five.times(2)
-        assertEquals(10, result.amount)
-        result = five.times(3)
-        assertEquals(15, result.amount)
+        assertEquals(Dollar(10), five.times(2))
+        assertEquals(Dollar(15), five.times(3))
+    }
+
+    @Test
+    fun testEquality() {
+        assertTrue(Dollar(5).equals(Dollar(5)))
+        assertFalse(Dollar(5).equals(Dollar(6)))
     }
 }
